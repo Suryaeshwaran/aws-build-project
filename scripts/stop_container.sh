8e299525a1a5f4215a097916077b72fe68ed1618
Suryaeshwaran <suryaeshwaran@gmail.com>
2024-09-13 21:35:44 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-# Get the list of running Docker containers
-running_containers=$(docker ps -q)
+container_id=$(sudo docker ps -q)
 
-# Check if there are any running containers
-if [ -z "$running_containers" ]; then
-    echo "No running Docker containers found."
-    exit 0
-fi
+sudo docker rm -f $container_id
 
-# Loop through each running container
-for container_id in $running_containers; do
-    # Check if the container is running
-    if [ "$(docker ps -q -f id=$container_id)" ]; then
-        echo "Stopping container with ID: $container_id"
-        docker stop $container_id
-    fi
-done
-
-echo "All running containers have been stopped."
+echo "Containers Stopped."
